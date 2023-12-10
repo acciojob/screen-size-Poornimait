d364449bc3af4b6693f42f9cba9b8ff25273c70b
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-10 20:46:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 //your JS code here. If required.
 function updateDimensions() {
-      // Get the width and height of the window
-      const width = window.innerWidth;
-      const height = window.innerHeight;
-
-      // Display the dimensions on the page
-      document.getElementById('width').textContent = width;
-      document.getElementById('height').textContent = height;
+      var width = window.innerWidth;
+      var height = window.innerHeight;
+	  var sizeInfo = document.querySelector('#sizeInfo h1');
+	 sizeInfo.textContent = 'Width: ' + width + ' and Height: ' + height;
+      
     }
 
-    // Initial call to set dimensions on page load
     updateDimensions();
-
-    // Add event listener for window resize
     window.addEventListener('resize', updateDimensions);
